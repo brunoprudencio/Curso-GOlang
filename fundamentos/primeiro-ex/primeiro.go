ebf96a0db066f03e7b1c76eda321fb41f3a9988e
Bruno Prudencio <brubatera5@gmail.com>
2020-01-05 16:15:05 -0300

    first commit

---
+++ fundamentos/primeiro-ex/primeiro.go
@@ -0,0 +1,15 @@
+//Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
